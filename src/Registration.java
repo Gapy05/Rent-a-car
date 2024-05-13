eccd93ba91e512d9f47579d6c46bc0aa41ba0841
Gapy05 <gapi.gorsek005@gmail.com>
2024-05-13 19:28:10 +0200

    dodan registration

--- src/Registration.java
+++ src/Registration.java
@@ -1,2 +1,266 @@
-package PACKAGE_NAME;public class Registration {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class MyFrame extends JFrame implements ActionListener {
+
+    // Components of the Form
+    private Container c;
+    private JLabel title;
+    private JLabel name;
+    private JTextField tname;
+    private JLabel email;
+    private JTextField temail;
+    private JLabel password;
+    private JPasswordField tpassword;
+    private JLabel mno;
+    private JTextField tmno;
+    private JLabel gender;
+    private JRadioButton male;
+    private JRadioButton female;
+    private ButtonGroup gengp;
+    private JLabel dob;
+    private JComboBox date;
+    private JComboBox month;
+    private JComboBox year;
+    private JLabel add;
+    private JTextArea tadd;
+    private JButton sub;
+    private JButton reset;
+    private JTextArea tout;
+    private JLabel res;
+    private JTextArea resadd;
+
+    private String datum[]
+            = { "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "10",
+            "11", "12", "13", "14", "15",
+            "16", "17", "18", "19", "20",
+            "21", "22", "23", "24", "25",
+            "26", "27", "28", "29", "30",
+            "31" };
+    private String meseci[]
+            = { "Jan", "feb", "Mar", "Apr",
+            "Maj", "Jun", "Jul", "Aug",
+            "Sep", "Okt", "Nov", "Dec" };
+    private String leta[]
+            = { "1995", "1996", "1997", "1998",
+            "1999", "2000", "2001", "2002",
+            "2003", "2004", "2005", "2006",
+            "2007", "2008", "2009", "2010",
+            "2011", "2012", "2013", "2014",
+            "2015", "2016", "2017", "2018",
+            "2019" };
+
+    public MyFrame() {
+        setTitle("Registracija");
+        setBounds(300, 90, 900, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+
+        c = getContentPane();
+        c.setLayout(null);
+
+        title = new JLabel("Registracija");
+        title.setFont(new Font("Arial", Font.PLAIN, 30));
+        title.setSize(300, 30);
+        title.setLocation(300, 30);
+        c.add(title);
+
+        name = new JLabel("Ime");
+        name.setFont(new Font("Arial", Font.PLAIN, 20));
+        name.setSize(100, 20);
+        name.setLocation(100, 100);
+        c.add(name);
+
+        tname = new JTextField();
+        tname.setFont(new Font("Arial", Font.PLAIN, 15));
+        tname.setSize(190, 20);
+        tname.setLocation(200, 100);
+        c.add(tname);
+
+        mno = new JLabel("Telefon");
+        mno.setFont(new Font("Arial", Font.PLAIN, 20));
+        mno.setSize(100, 20);
+        mno.setLocation(100, 150);
+        c.add(mno);
+
+        tmno = new JTextField();
+        tmno.setFont(new Font("Arial", Font.PLAIN, 15));
+        tmno.setSize(150, 20);
+        tmno.setLocation(200, 150);
+        c.add(tmno);
+
+        gender = new JLabel("Spol");
+        gender.setFont(new Font("Arial", Font.PLAIN, 20));
+        gender.setSize(100, 20);
+        gender.setLocation(100, 200);
+        c.add(gender);
+
+        male = new JRadioButton("Moški");
+        male.setFont(new Font("Arial", Font.PLAIN, 15));
+        male.setSelected(true);
+        male.setSize(75, 20);
+        male.setLocation(200, 200);
+        c.add(male);
+
+        female = new JRadioButton("Ženska");
+        female.setFont(new Font("Arial", Font.PLAIN, 15));
+        female.setSelected(false);
+        female.setSize(80, 20);
+        female.setLocation(275, 200);
+        c.add(female);
+
+        gengp = new ButtonGroup();
+        gengp.add(male);
+        gengp.add(female);
+
+        dob = new JLabel("Datum rojstva:");
+        dob.setFont(new Font("Arial", Font.PLAIN, 20));
+        dob.setSize(300, 20);
+        dob.setLocation(100, 250);
+        c.add(dob);
+
+        date = new JComboBox(datum);
+        date.setFont(new Font("Arial", Font.PLAIN, 15));
+        date.setSize(50, 20);
+        date.setLocation(250, 250);
+        c.add(date);
+
+        month = new JComboBox(meseci);
+        month.setFont(new Font("Arial", Font.PLAIN, 15));
+        month.setSize(60, 20);
+        month.setLocation(360, 250);
+        c.add(month);
+
+        year = new JComboBox(leta);
+        year.setFont(new Font("Arial", Font.PLAIN, 15));
+        year.setSize(80, 20);
+        year.setLocation(300, 250);
+        c.add(year);
+
+        add = new JLabel("Naslov");
+        add.setFont(new Font("Arial", Font.PLAIN, 20));
+        add.setSize(100, 20);
+        add.setLocation(100, 300);
+        c.add(add);
+
+        tadd = new JTextArea();
+        tadd.setFont(new Font("Arial", Font.PLAIN, 15));
+        tadd.setSize(200, 75);
+        tadd.setLocation(200, 300);
+        tadd.setLineWrap(true);
+        c.add(tadd);
+
+        email = new JLabel("Email");
+        email.setFont(new Font("Arial", Font.PLAIN, 20));
+        email.setSize(100, 20);
+        email.setLocation(100, 400);
+        c.add(email);
+
+        temail = new JTextField();
+        temail.setFont(new Font("Arial", Font.PLAIN, 15));
+        temail.setSize(190, 20);
+        temail.setLocation(200, 400);
+        c.add(temail);
+
+        password = new JLabel("Geslo");
+        password.setFont(new Font("Arial", Font.PLAIN, 20));
+        password.setSize(100, 20);
+        password.setLocation(100, 450);
+        c.add(password);
+
+        tpassword = new JPasswordField();
+        tpassword.setFont(new Font("Arial", Font.PLAIN, 15));
+        tpassword.setSize(190, 20);
+        tpassword.setLocation(200, 450);
+        c.add(tpassword);
+
+        sub = new JButton("Registriraj se!");
+        sub.setFont(new Font("Arial", Font.PLAIN, 15));
+        sub.setSize(200, 20);
+        sub.setLocation(150, 500);
+        sub.addActionListener(this);
+        c.add(sub);
+
+        reset = new JButton("Reset");
+        reset.setFont(new Font("Arial", Font.PLAIN, 15));
+        reset.setSize(100, 20);
+        reset.setLocation(380, 500);
+        reset.addActionListener(this);
+        c.add(reset);
+
+        tout = new JTextArea();
+        tout.setFont(new Font("Arial", Font.PLAIN, 15));
+        tout.setSize(300, 400);
+        tout.setLocation(500, 100);
+        tout.setLineWrap(true);
+        tout.setEditable(false);
+        c.add(tout);
+
+        res = new JLabel("");
+        res.setFont(new Font("Arial", Font.PLAIN, 20));
+        res.setSize(500, 25);
+        res.setLocation(100, 550);
+        c.add(res);
+
+        resadd = new JTextArea();
+        resadd.setFont(new Font("Arial", Font.PLAIN, 15));
+        resadd.setSize(200, 75);
+        resadd.setLocation(580, 175);
+        resadd.setLineWrap(true);
+        c.add(resadd);
+
+        setVisible(true);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == sub) {
+            String data1;
+            String data
+                    = "Name : "
+                    + tname.getText() + "\n"
+                    + "Mobile : "
+                    + tmno.getText() + "\n";
+            if (male.isSelected())
+                data1 = "Gender : Male"
+                        + "\n";
+            else
+                data1 = "Gender : Female"
+                        + "\n";
+            String data2
+                    = "DOB : "
+                    + (String)date.getSelectedItem()
+                    + "/" + (String)month.getSelectedItem()
+                    + "/" + (String)year.getSelectedItem()
+                    + "\n";
+
+            String data3 = "Address : " + tadd.getText();
+            tout.setText(data + data1 + data2 + data3);
+            tout.setEditable(false);
+            res.setText("Uspešno registriran!");
+            String uporabnisko_ime = tname.getText();
+
+        }
+
+        else if (e.getSource() == reset) {
+            String def = "";
+            tname.setText(def);
+            tadd.setText(def);
+            tmno.setText(def);
+            res.setText(def);
+            tout.setText(def);
+            date.setSelectedIndex(0);
+            month.setSelectedIndex(0);
+            year.setSelectedIndex(0);
+            resadd.setText(def);
+        }
+    }
+}
+
+class Registration {
+
+    public static void main(String[] args) throws Exception {
+        MyFrame f = new MyFrame();
+    }
 }
